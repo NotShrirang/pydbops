df6534eea467e7e7c46f4b62dacf925e6f8e7378
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-07 22:18:21 +0530

    Delete Database.py

--- Database.py
+++
@@ -1,8 +0,0 @@
-from DatabaseOperations import Database
-
-def openDatabase(filename:str):
-    """
-    Creates a database and returns a Database object.
-    """
-    d = Database(filepath=filename)
-    return d
