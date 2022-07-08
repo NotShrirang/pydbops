8b19c77f139528289661c72e1ee6e733d8968bc7
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-08 11:24:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-from DatabaseOperations import *
+# from DatabaseOperations import *
+import Database as Database
 
 if __name__ == "__main__":
+
+    # Creating Database instance.
+    d = Database.openDatabase("MyDB.db")
+
+    # Creating fields for table
     fields = {"Name" : "TEXT", "Number" : "INTEGER"}
-    d = openDatabase("MyDB.db")
-    d.createTable("Table1", fields=fields)
 
-    # print(d.addEntry("Table1", {"Name": "Shrirang", "Number" : 9404797231}))
-    # print(d.addEntry("Table1", {"Name": "Neil", "Number" : 2}))
-    # print(d.addEntry("Table1", {"Name": "Aai", "Number" : 9372074604}))
-    # print(d.addEntry("Table1", {"Name": "Baba", "Number" : 9371026821}))
+    # Creating table
+    d.createTable(tableName="Table1", fields=fields)
 
-    # d.createTable("Table2", fields={"Name" : "TEXT", "Number" : "INTEGER"})
-    # print(d.databaseVersion())
-    # print(d.tables())
-    # print(d.isEmpty())
-    # print(d.addEntry("Table1", {"Name": "Neil", "Number" : 5}))
-    # print(d.searchEntry(table="Table1", keyword="Neil", returnType="ids", findAllOccurence=True))
-    # d.removeEntry("table1", keyword="Neil" ,deleteAllOccurences=True)
-    # d.updateEntry(table="table1", values={"Number" : 6969}, field="Name", whereFieldIs="Neil")
+    # Creating Table instance
+    t1 = d.getTable(table="table1")
+
+    # Adding entry in table with Table instance.
+    t1.addEntry(values={"Name": "Shrirang", "Number" : 9404797231})
+
+    # Adding entry in table with Database instance.
+    d.addEntry(table="Table1", values={"Name": "Shrirang", "Number" : 9404797231})
+
+    # Searching entry with keyword.
+    d.searchEntry(table="Table1", keyword="Shrirang", returnType="ids", findAllOccurence=True)
+
+    # Updating an entry.
+    d.updateEntry(table="table1", values={"Number" : 123}, field="Name", whereFieldIs="Shrirang")
+
+    # Removing entry (or entries) with a keyword.
+    d.removeEntry("table1", keyword="Shrirang" ,deleteAllOccurences=True)
+
+    # Printing values of whole database.
+    print(d)
+
+    # Printing values of a table.
+    
+    print(t1)
+    
+    # Printing sqlite3 version
+    print(d.databaseVersion())
+
+    # Printing table names present in database.
+    print(d.tableNames())
+
+    # Printing length of the table.
+    print(d.length())
