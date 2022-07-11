237577ae0c028ccf520ca62a45de2647da967b4a
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-11 20:19:53 +0530

    Update test_pydbops.py

--- tests/test_pydbops.py
+++ tests/test_pydbops.py
@@ -1,12 +1,13 @@
 import pytest
-from src.pydbops.Database import *
-from src.pydbops.UserDefinedExceptions import InvalidReturnTypeError, InvalidParameterType
+from pydbops.Database import *
+from pydbops.UserDefinedExceptions import InvalidParameterTypeError, InvalidReturnTypeError, NoSuchTableError
 from pytest import *
 
 def test_Database_ops(db_conn: Database):
     d = db_conn
     assert type(d) is Database, f"Type of d is {type(d)}"
     assert d.createTable("Table1", {"Name" : "TEXT", "Character" : "TEXT"}) is True
+    assert d.databaseVersion() =="2.6.0"
     name_dict = {"Eddie" : "Joseph" ,"Steve" : "Joe", "Robin" : "Maya", "Dustin" : "Gaten", "Lucas" : "Caleb", "Mike" : "Finn", "Will" : "Noah", "Jane" : "Millie", "Nancy" : "Natalia"}
     i = 0
     for char, name in name_dict.items():
@@ -15,40 +16,54 @@ def test_Database_ops(db_conn: Database):
         assert m == i
     assert d.searchEntry(table="Table1", keyword="Steve", returnType="ids", findAllOccurence=False) == 2
     assert d.length() == 1
-    assert d.updateEntry(table="Table1", values={"Character" : "Eleven"}, field="Name", whereFieldIs="Millie") is True
+    assert d.updateEntry(table="Table1", values={"Character" : "Eleven"}, whereField="Name", Is="Millie") is True
     assert d.addEntry(table="Table1", values={"Name": "Jamie", "Character" : "Vecna"}) == i+1
-    assert d.removeEntry(table="Table1", keyword="Vecna", deleteAllOccurences=True) is True
+    assert d.removeEntry(table="Table1", keyword="Vecna", deleteAllOccurences=False) is True
     assert d.tableNames() == ["Table1"]
     assert d.tableNames(count=True) == 1
     assert d.getFieldNames(table="Table1", returnType="list") == ["Name", "Character"]
     assert d.getFieldNames(table="Table1", returnType="int") == 2
-    assert d.orderTable(table="Table1", field="Name") is True
-    assert d.orderTable(table="Table1", field="Name DESC") is True
-    assert d.orderTable(table="Table1", field=["Name DESC", "Character ASC"]) is True
-    assert d.orderTable(table="Table1", field={"Name" : "DESC", "Character" : "ASC"}) is True
+    assert d.fetchInOrder(table="Table1", field="Name") == [('Caleb', 'Lucas'), ('Finn', 'Mike'), ('Gaten', 'Dustin'), ('Joe', 'Steve'), ('Joseph', 'Eddie'), ('Maya', 'Robin'), ('Millie', 'Eleven'), ('Natalia', 'Nancy'), ('Noah', 'Will')]
+    assert d.fetchInOrder(table="Table1", field=["Name ASC", "Character ASC"]) == [('Caleb', 'Lucas'), ('Finn', 'Mike'), ('Gaten', 'Dustin'), ('Joe', 'Steve'), ('Joseph', 'Eddie'), ('Maya', 'Robin'), ('Millie', 'Eleven'), ('Natalia', 'Nancy'), ('Noah', 'Will')]
+    assert d.fetchInOrder(table="Table1", field={"Name" : "DESC", "Character" : "ASC"}) == [('Noah', 'Will'), ('Natalia', 'Nancy'), ('Millie', 'Eleven'), ('Maya', 'Robin'), ('Joseph', 'Eddie'), ('Joe', 'Steve'), ('Gaten', 'Dustin'), ('Finn', 'Mike'), ('Caleb', 'Lucas')]
+    assert d.addEntry(table="Table1", values={"Name": "Jamie", "Character" : "Vecna"}) == i+1
+    assert d.removeEntry(table="Table1", keyword="Vecna", deleteAllOccurences=True)
+    assert d.removeEntry(table="Table1", deleteAll=True)
+    d.addEntry(table="Table1", values={"Name": "Jamie", "Character" : "Vecna"})
+    records = d.dropTable("table1", getData=True)
+    assert records == [("Jamie", "Vecna"), ]
 
 def test_table_ops(db_conn: Database):
     db_conn.createTable("table2", {"Name" : "TEXT", "Character" : "TEXT"})
-    t1 = db_conn.getTable("table2")
-    assert type(t1) is Table, f"Type of t1 is {type(t1)}"
+    t2 = db_conn.getTable("table2")
+    assert type(t2) is Table, f"Type of t2 is {type(t2)}"
     value_dict = {"Name" : "Andy", "Character" : "Jake"}
-    assert t1.addEntry(values=value_dict) is 1
-    assert type(t1.databaseVersion()) is str
-    assert t1.getFieldNames(returnType="list") == ["Name", "Character"]
-    assert t1.length() is 1
-    assert t1.searchEntry(keyword="Jake", returnType="ids") is 1
-    assert t1.searchEntry(keyword="Jake", returnType="values") == ('Andy', 'Jake')
-    assert t1.updateEntry({"Name" : "Melissa", "Character" : "Amy"}, field="oid", whereFieldIs=1) is True
-    assert t1.removeEntry(id=1) is True
+    assert t2.addEntry(values=value_dict) == 1
+    assert type(t2.databaseVersion()) is str
+    assert t2.getFieldNames(returnType="list") == ["Name", "Character"]
+    assert t2.length() == 1
+    assert t2.databaseVersion() == "2.6.0"
+    assert t2.fetchInOrder(field="Name") == [("Andy", "Jake"),]
+    assert t2.fetchInOrder(field=["Name ASC", "Character ASC"]) == [("Andy", "Jake"),]
+    assert t2.fetchInOrder(field=["Name ASC", "Character ASC"]) == [("Andy", "Jake"),]
+    assert t2.searchEntry(keyword="Jake", returnType="ids") == 1
+    assert t2.searchEntry(keyword="Jake", returnType="values") == ('Andy', 'Jake')
+    assert t2.updateEntry({"Name" : "Melissa", "Character" : "Amy"}, whereField="oid", Is=1) is True
+    assert t2.removeEntry(id=1) is True
+    assert t2.removeEntry(keyword="Jake", deleteAllOccurences=True) is True
 
-def test_invalid_return_type(db_conn: Database):
+def test_errors(db_conn: Database):
     d = db_conn
     t2 = d.getTable("table2")
+    with pytest.raises(NoSuchTableError):
+        assert d.getTable("table1") == NoSuchTableError
+    d.createTable("table1", fields={"Name" : "TEXT", "Character" : "TEXT"})
     t1 = d.getTable("table1")
     with pytest.raises(InvalidReturnTypeError):
         assert d.getFieldNames(table="table1", returnType="in") == InvalidReturnTypeError
         assert t2.searchEntry(keyword="Jake", returnType="list") == InvalidReturnTypeError
-    with pytest.raises(InvalidParameterType):
-        assert t1.orderTable(field=1) == InvalidParameterType
+        assert d.tableNames(count=True, dictionary=True) == InvalidReturnTypeError
+    with pytest.raises(InvalidParameterTypeError):
+        assert t1.fetchInOrder(field=1) == InvalidParameterTypeError
     
 
