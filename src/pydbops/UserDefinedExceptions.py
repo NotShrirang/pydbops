5fefaec2ed3d412494c815ccc5d73c1d901aa08c
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-11 06:43:04 +0530

    Update UserDefinedExceptions.py

--- src/pydbops/UserDefinedExceptions.py
+++ src/pydbops/UserDefinedExceptions.py
@@ -1,6 +1,6 @@
 class InvalidReturnTypeError(Exception):
     """
-    Class for user defined exception.
+    Class for user defined exception, if return type mentioned is wrong.
     """
     def __init__(self, ReturnType, function) -> None:
         self.__returnType = ReturnType
@@ -16,3 +16,14 @@ class InvalidReturnTypeError(Exception):
         elif self.__function == "tableNames":
             self.__returnTypeList == ["int", "list", "dict"]
             return (f"\trReturn type cannot be two or more.\nDid you mean any one of {self.__returnTypeList} ?")
+
+class InvalidParameterType(Exception):
+    """
+    Class for user defined exception, if parameter type mentioned is wrong.
+    """
+    def __init__(self, argType, function) -> None:
+        self.__argType = argType
+        self.__function = function
+    def __str__(self) -> str:
+        return f"\tfield cannot be {self.__argType}"
+
