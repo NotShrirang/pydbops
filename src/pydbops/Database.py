c7e408c4e3c1dc430f248f61a47c86f3e572f3ee
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-11 20:19:39 +0530

    Update Database.py

--- src/pydbops/Database.py
+++ src/pydbops/Database.py
@@ -1,14 +1,29 @@
 from pydbops.pydbops import pydbops
 from pydbops.tables import Table
 from typing import overload
+from pydbops.UserDefinedExceptions import NoSuchTableError
 
 
 class Database(pydbops):
     """
-    Class for database operations.
+    Class for representing database
 
-    Args :
-        filepath (str): path for database file.
+    Args:
+    -----
+        - filepath (str) : path to the database
+
+    Methods:
+    -------
+        - addEntry() - Function for inserting values in table.
+        - createTable() - Creates table of given name.
+        - databaseVersion() - Returns sqlite3 version.
+        - fetchInOrder() - Function for fetching database entries in given order.
+        - getFieldNames() - Function for getting field names.
+        - length() - Returns length of table. Returns 0 if table is empty.
+        - removeEntry() - Function for removing records from table.
+        - searchEntry() - Function for searching in table.
+        - updateEntry() - Function for updating values in table.
+        - values() - Accesses records in a table.
     """
     def __init__(self, filepath: str) -> None:
         super().__init__(filepath)
@@ -48,7 +63,9 @@ class Database(pydbops):
         Returns:
             - True if executed.
         """
-        return super().createTable(tableName, fields)
+        ret = super().createTable(tableName, fields)
+        self.tables = self.tableNames(count=False, list=True, dictionary=False)
+        return ret
 
     def databaseVersion(self) -> str:
         """
@@ -56,11 +73,50 @@ class Database(pydbops):
         """
         return super().databaseVersion()
 
+    def dropTable(self, table: str, getData: bool = True) -> list[tuple[str | int, str | int, ]]:
+        """
+        Function for deleting table.
+
+        Args:
+            - table (str): Table name.
+            - getData (bool): If True, returns all the data before deleting the table.
+
+        Returns: list of records.
+        """
+        ret = super().dropTable(table=table, getData=getData)
+        self.tables = self.tableNames(count=False, list=True, dictionary=False)
+        return ret
+
     @overload
-    def getFieldNames(self, table: str, returnType: str = "int") -> int: ...
+    def fetchInOrder(self, table: str, field: list[str]) -> list[tuple[str | int, str | int, ]]: ...
+
+    @overload
+    def fetchInOrder(self, table: str, field: str) -> list[tuple[str | int, str | int, ]]: ...
+
+    @overload
+    def fetchInOrder(self, table: str, field: dict[str, str]) -> list[tuple[str | int, str | int, ]]: ...
+
+    def fetchInOrder(self, table: str, field: str | list[str] | dict[str, str]) -> list[tuple[str | int, str | int, ]]:
+        """
+        Function for fetching database entries in given order.
+
+        Args:
+            - table (str): Table name.
+            - field (str | list[str] | dict[str, str]):
+                    where order = [ASC, DESC])
+                    - str: "<field_name> <order>"
+                    - list[str]: ["<field_name> <order>", "<field_name> <order>", . . . ]
+                    - dict[str, str]: {"<field_name>" : "<order>", . . .}
+
+        Returns: list of records sorted in given order.
+        """
+        return super().fetchInOrder(table=table, field=field)
 
     @overload
     def getFieldNames(self, table: str, returnType: str = "list") -> list[str]: ...
+
+    @overload
+    def getFieldNames(self, table: str, returnType: str = "int") -> int: ...
 
     def getFieldNames(self, table: str, returnType: str = "list") -> int | list[str]:
         """
@@ -85,8 +141,11 @@ class Database(pydbops):
 
         Returns: Table
         """
-        t = Table(table=table, filepath=self.__filepath)
-        return t
+        if table in self.tables:
+            t = Table(table=table, filepath=self.__filepath)
+            return t
+        else:
+            raise(NoSuchTableError(table=table))
 
     def length(self) -> int:
         """
@@ -95,18 +154,6 @@ class Database(pydbops):
         Returns True if empty, else returns number of tables present.
         """
         return super().length()
-
-    @overload
-    def orderTable(self, table: str, field: list[str]) -> bool: ...
-
-    @overload
-    def orderTable(self, table: str, field: str) -> bool: ...
-
-    @overload
-    def orderTable(self, table: str, field: dict[str, str]) -> bool: ...
-
-    def orderTable(self, table: str, field: str | list[str] | dict[str, str]) -> bool:
-        return super().orderTable(table=table, field=field)
 
     def removeEntry(self, table: str, id: int = -1, keyword: str = "", deleteAllOccurences: bool = False, deleteAll: bool = False) -> bool:
         """
