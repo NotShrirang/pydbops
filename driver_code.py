f6afdc074efdade2b62907a4e8b45a4eece2cf47
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-14 22:02:09 +0530

    Update driver_code.py

--- driver_code.py
+++ driver_code.py
@@ -7,34 +7,42 @@ if __name__ == "__main__":
     d = openDatabase("MyDB.db")
 
     # Creating fields for table
-    fields = {"Name" : "TEXT", "Number" : "INTEGER"}
+    fields = {"Name" : "TEXT", "Character" : "TEXT"}
 
     # Creating table
     d.createTable(tableName="table1", fields=fields)
 
+    name_dict = {"Eddie" : "Joseph", "Steve" : "Joe", "Robin" : "Maya", "Dustin" : "Gaten", "Lucas" : "Caleb", "Mike" : "Finn", "Will" : "Noah", "Jane" : "Millie", "Nancy" : "Natalia"}
+    for char, name in name_dict.items():
+        # Adding entry in table with Database instance.
+        d.addEntry(table="Table1", values={"Name" : f"{name}", "Character" : f"{char}"})
+
+    # Creating table 2
+    d.createTable(tableName="Table2", fields={"Name" : "TEXT", "Number" : "INTEGER"})
+
     # Creating Table instance
-    t1 = d.getTable(table="table1")
+    t2 = d.getTable(table="Table2")
 
     # Adding entry in table with Table instance.
-    t1.addEntry(values={"Name": "Shrirang", "Number" : 9404797231})
+    t2.addEntry(values={"Name": "Shrirang", "Number" : 9404797231})
 
     # Adding entry in table with Database instance.
-    d.addEntry(table="Table1", values={"Name": "Shrirang", "Number" : 9404797231})
+    d.addEntry(table="Table2", values={"Name": "Shrirang", "Number" : 9404797231})
 
     # Searching entry with keyword.
-    d.searchEntry(table="Table1", keyword="Shrirang", returnType="ids", findAllOccurence=True)
+    d.searchEntry(table="Table2", keyword="Shrirang", returnType="ids", findAllOccurence=True)
 
     # Updating an entry.
-    d.updateEntry(table="table1", values={"Number" : 123}, whereField="Name", Is="Shrirang")
+    d.updateEntry(table="Table2", values={"Number" : 123}, whereField="Name", Is="Shrirang")
 
     # Printing values of whole database.
     print(d)
 
     # Printing values of a table.
-    print(t1)
+    print(t2)
     
     # Removing entry (or entries) with a keyword.
-    d.removeEntry(table="table1", keyword="Shrirang", deleteAllOccurences=True)
+    d.removeEntry(table="Table2", keyword="Shrirang", deleteAllOccurences=True)
     
     # Printing sqlite3 version
     print(d.databaseVersion())
