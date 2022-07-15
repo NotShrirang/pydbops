eaa57308040d9b5dec4065c95b6c77b2690f8c1d
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-15 06:11:34 +0530

    Update driver_code.py

--- driver_code.py
+++ driver_code.py
@@ -10,30 +10,32 @@ if __name__ == "__main__":
     fields = {"Name" : "TEXT", "Character" : "TEXT"}
 
     # Creating table
-    d.createTable(tableName="table1", fields=fields)
+    d.createTable(tableName="Table1", fields=fields)
 
     name_dict = {"Eddie" : "Joseph", "Steve" : "Joe", "Robin" : "Maya", "Dustin" : "Gaten", "Lucas" : "Caleb", "Mike" : "Finn", "Will" : "Noah", "Jane" : "Millie", "Nancy" : "Natalia"}
     for char, name in name_dict.items():
         # Adding entry in table with Database instance.
         d.addEntry(table="Table1", values={"Name" : f"{name}", "Character" : f"{char}"})
 
-    # Creating table 2
     d.createTable(tableName="Table2", fields={"Name" : "TEXT", "Number" : "INTEGER"})
+
+    # Creating an index
+    d.createIndex(indexName="MyIndex", tableName="Table1", columnName="Name", unique=False)
 
     # Creating Table instance
     t2 = d.getTable(table="Table2")
 
     # Adding entry in table with Table instance.
-    t2.addEntry(values={"Name": "Shrirang", "Number" : 9404797231})
+    t2.addEntry(values={"Name": "Shrirang", "Number" : 123456789})
 
     # Adding entry in table with Database instance.
-    d.addEntry(table="Table2", values={"Name": "Shrirang", "Number" : 9404797231})
+    d.addEntry(table="Table2", values={"Name": "Shrirang", "Number" : 123456789})
 
     # Searching entry with keyword.
     d.searchEntry(table="Table2", keyword="Shrirang", returnType="ids", findAllOccurence=True)
 
     # Updating an entry.
-    d.updateEntry(table="Table2", values={"Number" : 123}, whereField="Name", Is="Shrirang")
+    d.updateEntry(table="Table2", values={"Number" : 987654321}, whereField="Name", Is="Shrirang")
 
     # Printing values of whole database.
     print(d)
