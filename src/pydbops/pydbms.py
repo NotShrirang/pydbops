e958e9c461e3f1dc199f474c60c4a04c70b2e355
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-11-09 16:17:33 +0530

    added mongodb connectivity

--- src/pydbops/pydbms.py
+++ src/pydbops/pydbms.py
@@ -1,10 +1,21 @@
 from pydbops import *
-from platforms_names import *
-def openDatabase(system: str, filename: str, username: str = "", password: str = "") -> Database:
+from pydbops.database import Database
+import os
+import mysql.connector
+from pymongo import MongoClient
+
+class Databases():
+    SQLITE = 'sqlite3'
+    MYSQL = 'mysql'
+    POSTGRESQL = 'postgresql'
+    MONGO = 'mongodb'
+    FIREBASE = 'firebase'
+
+def openDatabase(system: str, filename: str = "", username: str = "", password: str = "") -> Database:
     """
     Creates a database and returns a Database object.
     """
-    if system == SQLITE:
+    if system == Databases.SQLITE:
         if filename[-3:] != ".db":
             raise(FileNotFoundError)
 
@@ -18,11 +29,14 @@ def openDatabase(system: str, filename: str, username: str = "", password: str =
             return d
         except FileNotFoundError:
             raise(FileNotFoundError(filename))
-        
-    elif system == MYSQL:
+    
+    elif system == Databases.MONGO:
+        return MongoClient("mongodb://localhost:27017")
+
+    elif system == Databases.MYSQL:
         mydb = mysql.connector.connect(
             host = "localhost",
             user = username,
             password = password
         )
-
+        return mydb
